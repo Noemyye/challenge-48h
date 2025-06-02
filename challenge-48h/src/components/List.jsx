4546a1f95d740e65e804eec0a7bf2f1ad928dd6c
dnoemie <noemie.dublanc@ynov.com>
2025-06-02 15:19:46 +0200

    initialization

---
+++ challenge-48h/src/components/List.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './List.css';
+import logo from '../assets/Logo_TBM.png'; // ← l'import est OBLIGATOIRE
+
+function List() {
+  return (
+    <div className="container-right">
+      <img 
+        src={logo}
+      />
+    </div>
+  );
+}
+
+export default List;
--- challenge-48h/src/main.jsx
+++ challenge-48h/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import List from './components/List.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <List/>
     <App />
   </StrictMode>,
 )
