bacb0ff247c18817e47d23fa74fbcb4f23ea2c29
dnoemie <noemie.dublanc@ynov.com>
2025-06-03 12:41:46 +0200

    start home page, header and list station

--- challenge-48h/src/App.jsx
+++ challenge-48h/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import './App.css';
+import 'leaflet/dist/leaflet.css';
+import List from './components/List';
+import MapBordeaux from './components/MapBordeaux';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className="main-layout">
+      <div className="header-row">
+        <List />
+        <MapBordeaux/>
+      </div>
+    </div>
   );
 }
 
